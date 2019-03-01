96ab8cc198a14cab18a35444674bc619b956f982
edelCustodio <edelcustodiofrias@gmail.com>
2019-03-01 18:08:06 -0500

    Primer commit para el autoupdate

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-// import { app, BrowserWindow } from 'electron';
 const { app, BrowserWindow } = require('electron');
-require('update-electron-app')();
+const { autoUpdater } = require("electron-updater");
+const log = require('electron-log');
+const dotenv  = require('dotenv');
+
+dotenv.config();
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+
 
 function createWindow() {
+    //check for updates
+    autoUpdater.checkForUpdatesAndNotify();
+    
     let win = new BrowserWindow({ width: 800, height: 600 });
     win.loadFile('index.html');
 
@@ -33,6 +43,44 @@ app.on('activate', () => {
         createWindow()
     }
 });
+
+/**
+ * Update app
+ */
+
+/**
+ * Envia notificacion a la pantalla principal
+ * acerca del status de la actualizacion
+ * @param {*} text Texto a enviar
+ */
+function sendStatusToWindow(text) {
+    let title = mainWindow.getTitle();
+    mainWindow.setTitle(title + ": " + text);
+    log.info(text);
+  }
+  
+  autoUpdater.on('checking-for-update', () => {
+    sendStatusToWindow('Checking for update...');
+  })
+  autoUpdater.on('update-available', (info) => {
+    sendStatusToWindow('Update available.');
+  })
+  autoUpdater.on('update-not-available', (info) => {
+    sendStatusToWindow('Update not available.');
+  })
+  autoUpdater.on('error', (err) => {
+    sendStatusToWindow('Error in auto-updater. ' + err);
+  })
+  autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+  })
+  autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update downloaded');
+  });
+  
   
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
